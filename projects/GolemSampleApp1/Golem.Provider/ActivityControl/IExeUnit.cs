7c44f080dfd217447dd15a68c97e60e5a5397b69
Mikolaj Barwicki <mikolaj.barwicki@gmail.com>
2019-11-24 14:42:09 +0100

    Comments added to IExeUnit

--- projects/GolemSampleApp1/Golem.Provider/ActivityControl/IExeUnit.cs
+++ projects/GolemSampleApp1/Golem.Provider/ActivityControl/IExeUnit.cs
@@ -19,6 +19,8 @@ namespace Golem.Provider.ActivityControl
 
     public interface IExeUnit
     {
+
+
         /// <summary>
         /// Activity related to this ExeUnit
         /// </summary>
@@ -26,6 +28,8 @@ namespace Golem.Provider.ActivityControl
 
         /// <summary>
         /// Gets called when Activity is created.
+        /// NOTE: The Activity parameter is expected to point to Agreement which includes all information required to initialize the ExeUnit, and to create activity later.
+        /// This means that validation needs to happen here.
         /// </summary>
         /// <param name="activity"></param>
         CreateActivityResult CreateActivity(Activity activity);
